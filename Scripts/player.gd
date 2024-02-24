85c4096d4acdcbe42866c8b5808084c69e851827
Joaco545 <joacoplac@gmail.com>
2024-02-24 03:59:15 -0300

    Refactored player to use states
    The player was refactored to use a state machine model for handling its movement state
    
    Crouching was also implemented as a state

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,21 +4,31 @@ extends CharacterBody3D
 const jumpVelocity = 7.0
 const baseSpeed = 5.0
 @export var sprintModifier = 1.3
+@export var crouchModifier = 0.8
+@export var airModifier = 1 # Set to 0 to disable airstrafe
 
 @export var smootherMovement:bool = true
 const lerpSpeed = 10.0
+
+var direction = Vector3.ZERO
 
 # Head movement
 @onready var head = $head
 @export var mouseSensitivity = 0.25
 
-# Random
-var direction = Vector3.ZERO
+# Collision/crouching
+@onready var standing_collider = $standing_collision_shape
+@onready var crouching_collider = $crouching_collision_shape
+@onready var headbump_raycast = $headbump_raycast
 
-#@onready var standing_collision_shape = $standing_collision_shape
+# States
+@onready var state = $player_state
+var state_list = ["walk", "run", "jump", "crouch"]
+var current_state = 0 # Starts on walk state
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -34,26 +44,46 @@ func _input(event):
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 		
 
+
 func _physics_process(delta):
-	# Reset speed to base
-	var currentSpeed = baseSpeed
-	
-	# Add the gravity.
+	# Add the gravity
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 		
 	
-	# Handle jump
-	if Input.is_action_just_pressed("jump") and is_on_floor(): #and not isCrouched:
-		velocity.y = jumpVelocity
+	var stateIdx = 0
+	
+	# Check inputs with priority: jump -> run -> crouch -> walk
+	if Input.is_action_pressed("jump"):
+		stateIdx = 2 # set jump
+		
+	elif Input.is_action_pressed("sprint"):
+		stateIdx = 1 # set run
+		
+	elif Input.is_action_pressed("crouch"):
+		stateIdx = 3 # set crouch
+		
+	else:
+		stateIdx = 0 # set walk
 		
 	
-	# Get the input direction and handle the movement/deceleration.
+	# If you are crouching, and you cant uncrouch
+	if current_state == 3 and not can_uncrouch():
+		stateIdx = 3 # Remain crouching
+		
+	
+	# If you are on the middle of a jump
+	if current_state == 2:
+		stateIdx = 2 # Remain jumping
+	
+	if(stateIdx != current_state):
+		state.send_event(state_list[stateIdx])
+		current_state = stateIdx
+		
+
+
+func move_player(speed: float, delta: float):
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	
-	if Input.is_action_pressed("sprint"):
-		currentSpeed *= sprintModifier
-		
 	
 	# Check if you want it snappy or smooth
 	if smootherMovement:
@@ -63,12 +93,67 @@ func _physics_process(delta):
 		direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		
 	
-	# Set the speed
 	if direction:
-		velocity.x = direction.x * currentSpeed
-		velocity.z = direction.z * currentSpeed
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, currentSpeed)
-		velocity.z = move_toward(velocity.z, 0, currentSpeed)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+		
 	
 	move_and_slide()
+
+
+func can_uncrouch() -> bool:
+	return not headbump_raycast.is_colliding()
+
+## STATE FUNCTIONS ##
+
+# On walk we move with base speed
+func _on_walk_state_physics_processing(delta):
+	move_player(baseSpeed, delta)
+
+
+# On run we move with sprint speed
+func _on_run_state_physics_processing(delta):
+	move_player(baseSpeed * sprintModifier, delta)
+
+
+# At the start of jumping, we set the speed
+func _on_jump_state_entered():
+	velocity.y = jumpVelocity
+
+
+# Move player if airstrafe modifier != 0
+# Check if we hit the ground and exit jump if we do
+func _on_jump_state_physics_processing(delta):
+	if(airModifier != 0):
+		move_player(baseSpeed * airModifier, delta)
+	
+	if is_on_floor():
+		current_state = 1 # Set state to walk
+		state.send_event(state_list[0])
+
+
+# Swap colliders to crouching
+# Turn on head raycast
+func _on_crouch_state_entered():
+	standing_collider.disabled = true
+	crouching_collider.disabled = false
+	headbump_raycast.enabled = true
+	
+
+
+# On crouch we move the player with crouch speed
+func _on_crouch_state_physics_processing(delta):
+	move_player(baseSpeed * crouchModifier, delta)
+	
+
+
+# Swap colliders to standing
+# Turn off head raycast
+func _on_crouch_state_exited():
+	standing_collider.disabled = false
+	crouching_collider.disabled = true
+	headbump_raycast.enabled = false
+	
